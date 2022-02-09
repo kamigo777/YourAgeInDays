4d6e0a573f09ace2d716c3a0737f35e7b398a08d
Igor <64247014+kamigo777@users.noreply.github.com>
2022-02-09 16:58:57 +0200

    plus Cat Generator

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
 function ageInDays() {
-    var birthYear = prompt( 'Ты когда родился выблядок?');
+    var birthYear = prompt('Ты когда родился друг?');
     var ageInDayss = (2022 - birthYear) * 365;
     var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode( 'Ты живешь уже ' + ageInDayss + ' дней на планете Земля');
+    var textAnswer = document.createTextNode('Ты живешь уже ' + ageInDayss + ' дней на планете Земля');
     h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
@@ -10,4 +10,13 @@ function ageInDays() {
 
 function reset() {
     document.getElementById('ageInDays').remove();
+}
+
+//Cat-Generator
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
 }
